266e329fdcaa556cc85fe10e43e4c59a7f71b591
Lars van Rhijn <larsvanrhijn@gmail.com>
2020-03-18 23:11:43 +0100

    Select subscriptions page added

--- website/kanikervanaf/settings.py
+++ website/kanikervanaf/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "kanikervanaf",
     "phonenumber_field",
     "subscriptions",
+    "users",
 ]
 
 MIDDLEWARE = [
---
+++ website/subscriptions/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import ListView
+
+urlpatterns = [
+    path(
+        "",
+        ListView.as_view(),
+        name="overview",
+    ),
+]
