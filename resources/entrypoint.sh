fa7d8f683b3caf3ad09f7cc463d2006ee9ced9ca
Lars van Rhijn <larsvanrhijn@gmail.com>
2020-03-29 11:18:02 +0200

    Entrypoint

---
+++ resources/entrypoint.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -e
+
+touch -a /kanikervanaf/log/uwsgi.log
+touch -a /kanikervanaf/log/django.log
+
+cd /kanikervanaf/src/website
+
+./manage.py migrate --no-input
+
+chown --recursive www-data:www-data /kanikervanaf/
+
+echo "Starting uwsgi server."
+uwsgi --chdir=/kanikervanaf/src/website \
+    --module=kanikervanaf.wsgi:application \
+    --master --pidfile=/tmp/project-master.pid \
+    --socket=:8000 \
+    --processes=5 \
+    --uid=www-data --gid=www-data \
+    --harakiri=20 \
+    --post-buffering=16384 \
+    --max-requests=5000 \
+    --thunder-lock \
+    --vacuum \
+    --logfile-chown \
+    --logto2=/kanikervanaf/log/uwsgi.log \
+    --ignore-sigpipe \
+    --ignore-write-errors \
+    --disable-write-exception
